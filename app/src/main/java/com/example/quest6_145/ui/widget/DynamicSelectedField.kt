cd7e1afaa90a14378c9c9bd277a03857efc9ff00
nrismawardah <nrismawardah@gmail.com>
2024-11-22 22:06:30 +0700

    Commit 17: Membuat function DynamicSelectedField untuk mengatur halaman rencana studi

---
+++ app/src/main/java/com/example/quest6_145/ui/widget/DynamicSelectedField.kt
@@ -0,0 +1,15 @@
+package com.example.quest6_145.ui.widget
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DynamicSelectedField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
