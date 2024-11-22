e78a7d110bc13f6c35b76ebcde3888f61a0f5955
nrismawardah <nrismawardah@gmail.com>
2024-11-22 23:14:50 +0700

    Commit 35: Membuat tampilan header untuk halaman tampil

--- app/src/main/java/com/example/quest6_145/ui/view/screen/TampilDataView.kt
+++ app/src/main/java/com/example/quest6_145/ui/view/screen/TampilDataView.kt
@@ -1,7 +1,24 @@
 package com.example.quest6_145.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quest6_145.R
 import com.example.quest6_145.ui.model.Mahasiswa
 import com.example.quest6_145.ui.model.RencanaStudi
 
@@ -11,5 +28,46 @@ fun TampilDataView(
     mhsState: Mahasiswa,
     rstdState: RencanaStudi
 ){
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary)
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(
+            modifier = Modifier.padding(16.dp)
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(5.dp)
+            )
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Yellow,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(5.dp)
+                )
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Yellow,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(5.dp)
+                )
+                Spacer(
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+        }
+    }
 }
