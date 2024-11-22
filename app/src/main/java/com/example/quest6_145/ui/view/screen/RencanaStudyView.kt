5475fdad53bbcb76ea3975c156f259717eefb8f6
nrismawardah <nrismawardah@gmail.com>
2024-11-22 22:01:27 +0700

    Commit 9: Membuat header pada tampilan halaman rencana studi menggunakan elemen spacer, image, serta text

--- app/src/main/java/com/example/quest6_145/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/quest6_145/ui/view/screen/RencanaStudyView.kt
@@ -1,19 +1,82 @@
 package com.example.quest6_145.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quest6_145.R
 import com.example.quest6_145.ui.model.Mahasiswa
 
+@Preview(showBackground = true)
 @Composable
 fun RencanaStudyView(
-    mahasiswa: Mahasiswa,
+
 ){
     var chosenDropdown by remember { mutableStateOf("") }
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
 
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary)
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(
+            modifier = Modifier.padding(16.dp)
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(5.dp)
+            )
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Yellow,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(5.dp)
+                )
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Yellow,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(5.dp)
+                )
+                Spacer(
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+        }
+    }
 }
