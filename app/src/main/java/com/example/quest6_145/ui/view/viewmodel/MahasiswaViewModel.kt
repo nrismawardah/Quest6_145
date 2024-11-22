787f17cbcaafe4f4ee0dbcd604498cd69a47f8a4
nrismawardah <nrismawardah@gmail.com>
2024-11-22 22:29:06 +0700

    Commit 21: Membuat function saveDataMahasiswa untuk memperbarui state mahasiswa dengan data baru

--- app/src/main/java/com/example/quest6_145/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/quest6_145/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -5,9 +5,20 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import com.example.quest6_145.ui.model.Mahasiswa
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel : ViewModel()
 {
     private val _mahasiswaUiState = MutableStateFlow(Mahasiswa())
     val mahasiswaUiState: StateFlow<Mahasiswa> = _mahasiswaUiState.asStateFlow()
+
+    fun saveDataMahasiswa(mahasiswa: Mahasiswa) {
+        _mahasiswaUiState.update { currentState ->
+            currentState.copy(
+                nim = mahasiswa.nim,
+                nama = mahasiswa.nama,
+                email = mahasiswa.email
+            )
+        }
+    }
 }
