abf313637650b48ad0a5f324d09098307ac22475
nrismawardah <nrismawardah@gmail.com>
2024-11-22 20:57:00 +0700

    Commit 5: Menambahkan layout column yang memenuhi layar secara vertikal dan memusatkan elemen-elemennya ke tengah, serta mengatur warna background

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.quest6_145"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.quest6_145"
---
+++ app/src/main/java/com/example/quest6_145/ui/view/screen/SplashView.kt
@@ -0,0 +1,30 @@
+package com.example.quest6_145.ui.view.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quest6_145.R
+
+@Preview (showBackground = true)
+@Composable
+fun SplashView(
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+    }
+}
