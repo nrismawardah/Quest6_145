0d8db56068bf23945ed1dd0c35ac707366155044
nrismawardah <nrismawardah@gmail.com>
2024-11-22 22:37:45 +0700

    Commit 23: Membuat function setMataKuliah untuk memperbarui namaMK pada state _krsState

--- app/src/main/java/com/example/quest6_145/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/quest6_145/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -5,9 +5,14 @@ import com.example.quest6_145.ui.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel()
 {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
 }
