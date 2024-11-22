466fb37050fae344d02de10bc21ad63d5c9cd85e
nrismawardah <nrismawardah@gmail.com>
2024-11-22 21:46:34 +0700

    Commit 11: Membuat list data pilihan matakuliah untuk halaman rencana studi menggunakan variable val

---
+++ app/src/main/java/com/example/quest6_145/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.quest6_145.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
+}
