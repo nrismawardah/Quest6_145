a084cf1615f43b7660855345fd5ae0c02a4d2e1e
nrismawardah <nrismawardah@gmail.com>
2024-11-22 22:43:16 +0700

    Commit 27: Membuat function MahasiswaApp yang akan menampilkan dan mengatur semua halaman pada aplikasi

--- app/src/main/java/com/example/quest6_145/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/quest6_145/navigasi/PengelolaHalaman.kt
@@ -1,8 +1,24 @@
 package com.example.quest6_145.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Halaman {
     Splash,
     Mahasiswa,
     Matakuliah,
     Tampil,
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
+
 }
