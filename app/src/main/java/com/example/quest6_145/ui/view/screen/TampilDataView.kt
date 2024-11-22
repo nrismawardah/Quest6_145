fac20c09b898a70f40771be86dfa10e5cccc427d
nrismawardah <nrismawardah@gmail.com>
2024-11-22 23:18:17 +0700

    Commit 36: Membuat function untuk menampilkan data pada halaman tampil

--- app/src/main/java/com/example/quest6_145/ui/view/screen/TampilDataView.kt
+++ app/src/main/java/com/example/quest6_145/ui/view/screen/TampilDataView.kt
@@ -2,12 +2,16 @@ package com.example.quest6_145.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -68,6 +72,39 @@ fun TampilDataView(
                     modifier = Modifier.padding(10.dp)
                 )
             }
+        }
+        Spacer(
+            modifier = Modifier
+                .padding(10.dp)
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(topEnd = 15.dp, topStart = 15.dp)
+                )
+        ){
+
+        }
+    }
+}
+
+@Composable
+fun TampilData( param:String, argu:String){
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(text = param,
+                modifier = Modifier.weight(0.8f))
+            Text(text = ": ",
+                modifier = Modifier.weight(0.2f))
+            Text(text = argu,
+                modifier = Modifier.weight(1f))
         }
     }
 }
