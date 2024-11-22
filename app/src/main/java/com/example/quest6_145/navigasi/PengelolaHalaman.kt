9382f8e4bdbce54b0dd60c5286ce0b80b97d68dd
nrismawardah <nrismawardah@gmail.com>
2024-11-22 22:46:38 +0700

    Commit 29: Menambahkan elemen NavHost untuk mengatur navigasi dan mengatur agar aplikasi dimulai dengan halaman Splash

--- app/src/main/java/com/example/quest6_145/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/quest6_145/navigasi/PengelolaHalaman.kt
@@ -1,11 +1,13 @@
 package com.example.quest6_145.navigasi
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.quest6_145.ui.view.viewmodel.MahasiswaViewModel
 import com.example.quest6_145.ui.view.viewmodel.RencanaStudyViewModel
@@ -26,4 +28,12 @@ fun MahasiswaApp(
 ){
     val mahasiswaUiState by mahasiswaViewModel.mahasiswaUiState.collectAsState()
     val krsStateUi by krsViewModel.krsStateUi.collectAsState()
+
+    NavHost(
+        modifier = modifier.padding(),
+        navController = navController,
+        startDestination = Halaman.Splash.name
+    ){
+
+    }
 }
